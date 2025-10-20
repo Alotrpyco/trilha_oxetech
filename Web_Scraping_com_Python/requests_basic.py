a7bd5999ef7d4ed07f56f80d77904ffaf22f463b
Sérgio Ricardo <working7.sergio@gmail.com>
2025-10-20 00:00:34 -0300

    Update

--- Web_Scraping_com_Python/requests_basic.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-def fazer_requisicao(url:str) -> str:
-    resultado = requests.get(url)
-    return resultado
-
-if __name__ == "__main__":
-    url = "https://google.com"
-    resultado = fazer_requisicao(url)
